485695444fbd79972905bb059d74c505d8788d1e
Raúl Casado San Andrés <raulcasadosanandres1@gmail.com>
2026-02-22 19:10:26 +0100

    index.php de aplicacion simple, fecha y hola mundo

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Pipeline Jenkins + Azure</title>
+</head>
+<body>
+    <h1>¡Hola Mundo! Despliegue automatizado con Jenkins y Azure </h1>
+    <p>La fecha y hora actual en el servidor es: <?php echo date('Y-m-d H:i:s'); ?></p>
+    <p>Versión de PHP: <?php echo phpversion(); ?></p>
+</body>
+</html>
